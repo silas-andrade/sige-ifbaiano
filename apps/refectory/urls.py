1279535fe8fba6ef4bf7f7f466d0ed18e9b37764
Silas Andrade <silasandradee@outlook.com>
2026-02-04 18:21:26 -0300

    Retirei a rota 'refectory/queue-status' por não está sendo usada

--- apps/refectory/urls.py
+++ apps/refectory/urls.py
@@ -9,10 +9,10 @@ from .views import (
 
 app_name = 'refectory'
 
-
 urlpatterns = [
     path("", refectory_page, name="home"),
     path("create-token/", create_token, name="create-token"),
     path("validate-token/<uuid:pk>", validate_token, name="validate-token"),
     path("scan-token/<uuid:pk>/", scan_token, name="scan-token"),
-    path('queue-status/', queue_status, name='queue-status'),]
+    #path('queue-status/', queue_status, name='queue-status'),
+]
